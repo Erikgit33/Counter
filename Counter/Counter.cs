90b9d9b64f0fa4aec6abd94e65b9d78d53a6b201
Erik Bäckström <Erik Bckstrm@DESKTOP-MEVT6FK>
2024-09-11 09:34:31 +0200

    Add project files.

---
+++ Counter/Counter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    internal class Counter
+    {
+        int count;
+        //int start;
+
+        public Counter(int startValue)
+        {
+            //start = startValue;
+            count = startValue;
+        }
+
+        public void Increment()
+        {
+            count++;
+        }
+
+        public void IncrementBy(int value)
+        {
+            count += value;
+        }
+
+        public void Reset()
+        {
+            count = 0;
+        }
+
+        public string GetCount()
+        {
+            return count.ToString();
+        }
+    }
+}
---
+++ Counter/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Counter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter(7); //Start value
+
+            counter.Increment();
+            counter.Increment();
+            counter.Increment();
+
+            Console.WriteLine("Counter: " + counter.GetCount());
+            counter.Reset();
+
+            Console.WriteLine("Counter: " + counter.GetCount());
+            counter.IncrementBy(5);
+
+            Console.WriteLine("Counter: " + counter.GetCount());
+        }
+    }
+}
